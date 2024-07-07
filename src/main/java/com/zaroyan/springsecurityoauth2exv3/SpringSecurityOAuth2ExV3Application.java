946d3fc580ca63b1034babc6304cdd0a90438daa
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-07-07 19:55:08 +0300

    "initial commit"

---
+++ src/main/java/com/zaroyan/springsecurityoauth2exv3/SpringSecurityOAuth2ExV3Application.java
@@ -0,0 +1,13 @@
+package com.zaroyan.springsecurityoauth2exv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2ExV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOAuth2ExV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaroyan/springsecurityoauth2exv3/SpringSecurityOAuth2ExV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaroyan.springsecurityoauth2exv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2ExV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
